9434ba6bf9696c770286ccc54c3e43791c8ef51d
izabelakatarzyna <kolodziejczyk.swift@gmail.com>
2022-09-28 20:53:44 +0200

    table done

--- script.js
+++ script.js
@@ -4,9 +4,10 @@ document.querySelectorAll(".project").forEach((project) => {
     document.querySelector(".display h3").innerText =
       project.querySelector("h3").innerTxt;
     document.querySelector(".display p").innerText =
-      project.querySelector("p").innerTxt;
+      project.querySelector("p").innerText;
     document.querySelector(".display img").src =
       project.querySelector("img").src;
     document.querySelector(".display a").href = project.querySelector("a").href;
   };
 });
+document.querySelector(".project").click();
